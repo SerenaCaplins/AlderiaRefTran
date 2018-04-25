83571765670d30c1cd6e00da82fdf6d86cc61b28
SerenaCaplins <sacaplins@ucdavis.edu>
2018-04-25 15:47:12 -0700

    Update diginorm.sh

--- diginorm.sh
+++ diginorm.sh
@@ -1,7 +1,17 @@
 
 
-
+#from http://dibsi-rnaseq.readthedocs.io/en/latest/assembly-trinity.html
+#normalizes based on kmers and read counts
 
 
 module load khmer
+
+normalize-by-median.py -p -k 20 -C 20 -M 4e9 \
+  --savegraph normC20k20.ct -u orphans.qc.fq.gz \
+  *.pe.qc.fq.gz
+  
+  
+
+
+
 
